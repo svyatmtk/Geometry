40f8dc22a86b394b0cfac11a4bbe54e96eec1434
svyatmtk <jesusvoskres0@gmail.com>
2024-04-11 19:55:27 +0300

    Добавьте файлы проекта.

---
+++ Geometry/GeometryTests.cs
@@ -0,0 +1,55 @@
+﻿using Geometry;
+using Xunit;
+
+public class GeometryTests
+{
+    [Fact]
+    public void Circle_CalculateArea_ValidRadius_ReturnsCorrectArea()
+    {
+        //Arrange
+        double radius = 5;
+        Circle circle = new Circle(radius);
+
+        //Act
+        double circleArea = circle.CalculateArea();
+
+        //Assert
+        double expectedArea = Math.PI * Math.Pow(radius, 2);
+        Assert.Equal(expectedArea, circleArea);
+    }
+
+    [Fact]
+    public void Triangle_CalculateArea_ValidSides_ReturnsCorrectArea()
+    {
+        // Arrange
+        double side1 = 3;
+        double side2 = 4;
+        double side3 = 5;
+        Triangle triangle = new Triangle(side1, side2, side3);
+
+        // Act
+        double actualArea = triangle.CalculateArea();
+
+        // Assert
+        double expectedArea = 6;
+        Assert.Equal(expectedArea, actualArea);
+    }
+
+    [Fact]
+    public void Triangle_IsRightTriangle_RightTriangle_ReturnsTrue()
+    {
+        // Arrange
+        double side1 = 3;
+        double side2 = 4;
+        double side3 = 5;
+        Triangle triangle = new Triangle(side1, side2, side3);
+
+        // Act
+        bool isRightTriangle = triangle.IsRightTriangle();
+
+        // Assert
+        Assert.True(isRightTriangle);
+    }
+
+}
+
---
+++ Geometry/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Geometry
+{
+    internal interface IFigure
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Радиус должен быть больше 0");
+            }
+            Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IFigure
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            if (side1 < 0 || side2 < 0 || side3 < 0)
+            {
+                throw new ArgumentException("Стороны должны быть больше 0");
+            }
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+        public double CalculateArea()
+        {
+            double semiPerimeter = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - Side1) * (semiPerimeter - Side2) * (semiPerimeter - Side3));
+        }
+        public bool IsRightTriangle()
+        {
+            double[] sides = { Side1, Side2, Side3 };
+            Array.Sort(sides);
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
